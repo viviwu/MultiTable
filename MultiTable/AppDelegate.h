92dfc799fca2fa017757bedb0e6c0d75db9cbf05
xcodevivi <vivi705wu@sina.com>
2015-08-30 21:56:51 +0800

    Initial Commit

---
+++ MultiTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiTable
+//
+//  Created by viviwu on 15/8/30.
+//  Copyright (c) 2015年 viviwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiTable
+//
+//  Created by viviwu on 15/8/30.
+//  Copyright (c) 2015年 viviwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
